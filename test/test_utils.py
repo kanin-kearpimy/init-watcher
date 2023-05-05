940f695610e9f21ae06dcce9525b87598f782e6f
kanin-kearpimy <kanin.kearpimy@gmail.com>
2023-05-05 14:48:21 +0700

    describe function in test_tuils

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,26 +1,47 @@
 import os
 from src.initwatcher import utils
 
+current_dir = os.getcwd()
+
 
 def test_exclude_directory():
+    """
+    is_exclude_directory()
+    should return True when __pycache__/ is in paths
+    Because it's excluded
+    """
     expect_result = True
     is_exclude_dir = utils.is_exclude_directory("root/__pycache__/abc")
     assert expect_result == is_exclude_dir
 
 
 def test_exclude_test_directory():
+    """
+    is_exclude_directory()
+    should return True when test/ is in paths
+    Because it's excluded
+    """
     expect_result = True
     is_exclude_dir = utils.is_exclude_directory("root/123/www/aaa/test/ass/brtyyi/q")
     assert expect_result == is_exclude_dir
 
 
 def test_not_exclude_directory():
+    """
+    is_exclude_directory()
+    should return False when facing root/package_a (not excluded)
+    """
     expect_result = False
     is_exclude_dir = utils.is_exclude_directory("root/package_a")
     assert expect_result == is_exclude_dir
 
 
 def test_different_directories():
+    """
+    compare_directories()
+    should return root/package_B due to differences
+    between old and new directories system
+    """
     old_directory = ["root/a/b", "root/package_T"]
     new_directory = ["root/a/b", "root/package_T", "root/package_B"]
     expect_result = ["root/package_B"]
@@ -30,6 +51,14 @@ def test_different_directories():
 
 
 def test_different_2_directories():
+    """
+    compare_directories()
+    should return root/package_B due to differences
+    between old and new directories system
+
+    Nothing to do with "root/a/b/c/d/e/f"
+    because it's deleted
+    """
     old_directory = ["root/a/b", "root/package_T", "root/a/b/c/d/e/f"]
     new_directory = ["root/a/b", "root/package_T", "root/package_B"]
     expect_result = ["root/package_B"]
@@ -39,6 +68,11 @@ def test_different_2_directories():
 
 
 def test_no_diffrent_directories():
+    """
+    compare_directories()
+    should return [] due to no differences
+    between old and new directories system
+    """
     old_directory = ["root/a/b"]
     new_directory = ["root/a/b"]
     expect_result = []
@@ -48,16 +82,25 @@ def test_no_diffrent_directories():
 
 
 def test_not_create_init_file():
-    paths = ["{}/test/utils".format(os.getcwd())]
+    """
+    create_init_file()
+    should not create __init__.py in /test/utils
+    because test/ is excluded
+    """
+    paths = [f"{current_dir}/test/utils"]
     utils.create_init_file(paths)
     assert "__init__.py" not in os.listdir(paths[0])
 
 
 def test_create_init_file():
-    directory = "{}/__job__".format(os.getcwd())
+    """
+    create_init_file()
+    should create __init__.py in /test/utils
+    """
+    directory = f"{current_dir}/__job__"
     os.makedirs(directory)
     paths = [directory]
     utils.create_init_file(paths)
     assert "__init__.py" in os.listdir(paths[0])
-    os.remove("{}/__init__.py".format(directory))
+    os.remove(f"{directory}/__init__.py")
     os.rmdir(directory)
